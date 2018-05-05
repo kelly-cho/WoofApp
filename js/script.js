129b78adce71058a1b52cfe40f95c0c37a96ae79
kelly-cho <wenhsiu55@gmail.com>
2018-05-05 01:44:36 -0700

    SubBreed Filter

--- js/script.js
+++ js/script.js
@@ -10,16 +10,12 @@ function loadBreed()
 
         $.each (breeds, function(name, subbreed)
         {
-            // if no sub breed
-            if (breeds[name].length == 0) 
-            {
-                $('#breed-selector').append('<option>' + name + '</option>');
-            }
-            else
+            $('#breed-selector').append('<option>' + name + '</option>');
+            /*else
             {
                 for (n = 0; n < breeds[name].length; n ++) 
                     $('#breed-selector').append('<option>'+ breeds[name][n] + ' ' + name + '</option>');
-            }
+            }*/
 
         });
     });    
@@ -33,15 +29,10 @@ function loadImages()
     $('#grid').html(''); 
 
     var selected = $('#breed-selector').val();
-    var $title = $('#title');
-    $title.text(selected);
-
     var images = 'https://dog.ceo/api/breed/' + selected + '/images';
 
     $.getJSON (images, function(data)
     {
-        var urls = data.message;
-
         for (n = 0; n < data.message.length; n ++)
         {
             // boostrap 4 masonry card gallery
@@ -50,9 +41,64 @@ function loadImages()
 
     }); 
 
+    checkSubBreed();
+
+};
+
+// check for sub-breed
+function checkSubBreed()
+{
+    var selected = $('#breed-selector').val();    
+
+    var subs = 'https://dog.ceo/api/breed/' + selected + '/list';
+
+    $.getJSON (subs, function(data)
+    {
+
+        if (data.message.length == 0)
+            $('#subbreed-selector').hide();
+        else
+        {
+            $('#subbreed-selector').html('');
+            $('#subbreed-selector').append('<option selected disabled>Select a SubBreed...</option>');
+            $('#subbreed-selector').show();
+
+            for (n = 0; n < data.message.length; n ++)
+            {
+                $('#subbreed-selector').append('<option>' + data.message[n] + '</option>');
+            }
+        }
+
+    }); 
+
+}
+
+// load images of selected sub-breed
+function loadImagesSub()
+{
+    // clear grid
+    $('#grid').html(''); 
+
+    var breed    = $('#breed-selector').val();
+    var subbreed = $('#subbreed-selector').val();
+
+    var images = 'https://dog.ceo/api/breed/' + breed + '/' + subbreed + '/images';
+
+    $.getJSON (images, function(data)
+    {
+        for (n = 0; n < data.message.length; n ++)
+        {
+            // boostrap 4 masonry card gallery
+            $('#grid').append('<div class="card"><img class="card-img" src="' + data.message[n] + '"></div>');            
+        }
+
+    }); 
 
 };
 
 
 $('#breed-selector').change(loadImages);
+$('#subbreed-selector').change(loadImagesSub);
 
+//var $title = $('#title');
+//$title.text(selected);
